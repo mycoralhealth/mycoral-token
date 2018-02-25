d5b8d160dd2fc0f031eeda9493c867ed5a9f3f62
kriscosta <kristoncosta@gmail.com>
2018-02-25 01:01:27 -0500

    Renaming the token

--- contracts/MyCoralToken.sol
+++ contracts/MyCoralToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.18;
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 contract MyCoralToken is MintableToken {
-  string public constant name     = "MyCoralHealthToken";
-  string public constant symbol   = "MCHT";
+  string public constant name     = "Coral Health Token";
+  string public constant symbol   = "CHT";
   uint8  public constant decimals = 18;
 }
